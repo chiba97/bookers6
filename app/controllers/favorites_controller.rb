e3009e1ea81771f4ea5986158dd32bf89c82fee0
chiba97 <ktfkyrya@gmail.com>
2021-09-15 10:22:06 +0000

    [Add]いいね機能非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
     book_id = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book_id.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+    @book = favorite.book
   end
   
   def destroy
     book_id = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book_id.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    @book = favorite.book
   end
   
 end
